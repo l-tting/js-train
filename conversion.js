3942fd8cff6ba59b26017a68d1947aae60dce6c8
l-tting <brianletting01@gmail.com>
2025-11-12 20:45:04 +0300

    operators

---
+++ conversion.js
@@ -0,0 +1,35 @@
+//string to number
+let num = "23.5"
+
+let x = Number(num)
+let y = parseFloat(num)
+let z = parseInt(num)
+
+console.log(num,typeof(num))
+console.log(x,typeof(x)) //23.5 number
+console.log(y,typeof(y)) //23.5 number
+console.log(z,typeof(z)) //23 number
+
+//number to string
+let a = 598
+let b = String(a)
+let c = a.toString()
+
+console.log(a,typeof(a))
+console.log(b,typeof(b))
+console.log(c,typeof(c))
+
+
+//type coercion
+let value1 = "5"
+let value2 = "12"//-> "12"
+console.log(value1 / value2) //512
+
+let f = "Jane"
+let g = "Doe"
+
+
+
+        
+
+
---
+++ operators.js
@@ -0,0 +1,17 @@
+console.log("5"==5) // type coercion
+console.log("5"===5) // no type coercion 
+
+
+let x = 5
+let y = 10
+let z = x + y
+console.log(z) //15
+
+
+let a = 5
+a+=10
+console.log(a) //15
+
+// a = 5
+// a + 10 = 15
+// a = 15
